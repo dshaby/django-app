51c68f383034c04771ca28c8e410927dfcdb88d7
Daniel Shaby <dshaby@berkeley.edu>
2024-06-21 12:48:28 -0700

    converts functional views to class views

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from typing import Any
+from django.views.generic import DetailView, ListView
 from .models import Post
 
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
 
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
-
-
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
-    })
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
 
-def post_detail(request, slug):
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post,
-        "post_tags": identified_post.tags.all()
-    })
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/all-posts.html"
+    context_object_name = "all_posts"
+    ordering = ["-date"]
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post-detail.html"
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
