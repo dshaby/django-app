16f4aeef1aa1c7c97e7edbb251b1e0e7ef6f3ee0
Daniel Shaby <dshaby@berkeley.edu>
2024-06-21 18:40:02 -0700

    adds 'read later' feature w/ sessions

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting-page"),
     path("posts", views.PostListView.as_view(), name="posts-page"),
-    path("posts/<slug:slug>", views.PostDetailView.as_view(), name="post-detail-page")
+    path("posts/<slug:slug>", views.PostDetailView.as_view(),
+         name="post-detail-page"),
+    path("read-later", views.ReadLaterView.as_view(), name="read-later")
 ]
