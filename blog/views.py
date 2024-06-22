2d52731639665b0631defce6f9bc510f823bcc73
Daniel Shaby <dshaby@berkeley.edu>
2024-06-21 17:35:05 -0700

    makes comments visible to the user

--- blog/views.py
+++ blog/views.py
@@ -30,7 +30,8 @@ class PostDetailView(View):
         context = {
             "post": post,
             "post_tags": post.tags.all(),
-            "comment_form": CommentForm()
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id")
         }
         return render(request, "blog/post-detail.html", context)
 
@@ -48,7 +49,8 @@ class PostDetailView(View):
         context = {
             "post": post,
             "post_tags": post.tags.all(),
-            "comment_form": comment_form
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id")
         }
 
         return render(request, "blog/post-detail.html", context)
