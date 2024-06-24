81947415c0954acea5d95a963a579401f6f7e5b1
Daniel Shaby <dshaby@berkeley.edu>
2024-06-23 18:23:43 -0700

    adds postgres configuration

--- django_app/settings.py
+++ django_app/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': getenv('DATABASE_USERNAME', os.getenv("DATABASE_USERNAME")),
+        'PASSWORD': getenv('DATABASE_PASSWORD', os.getenv("DATABASE_PASSWORD")),
+        'HOST': getenv('DATABASE_HOST', os.getenv("DATABASE_HOST")),
+        'PORT': '5432',
     }
 }
 
